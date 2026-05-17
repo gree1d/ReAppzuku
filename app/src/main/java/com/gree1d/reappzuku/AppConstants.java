008f3b1ed5029855506313d5c137a2e740da5ce1
gree1d <161749879+gree1d@users.noreply.github.com>
2026-05-17 11:36:25 +0400

    Update AppConstants.java

--- app/src/main/java/com/gree1d/reappzuku/AppConstants.java
+++ app/src/main/java/com/gree1d/reappzuku/AppConstants.java
@@ -15,7 +15,7 @@ public final class AppConstants {
         // Stats & History
         public static final long STATS_HISTORY_DURATION_MS = 12 * 60 * 60 * 1000L;
         public static final int STATS_MAX_COUNT = 10_000;
-        public static final int RELAUNCH_GREEDY_THRESHOLD = 3
+        public static final int RELAUNCH_GREEDY_THRESHOLD = 3;
 
         // Delays
         public static final int RELAUNCH_CHECK_DELAY_MS = 2000;
